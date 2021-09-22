721936c341b7080bda03301f94be37eba0a981ea
pmecchia <mecchia.pierre@gmail.com>
2021-09-22 20:01:29 +0200

    Exercices session1

--- session_1/wordcount.py
+++ session_1/wordcount.py
@@ -81,7 +81,7 @@ def print_top(filename):
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print ('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
   option = sys.argv[1]
@@ -91,7 +91,7 @@ def main():
   elif option == '--topcount':
     print_top(filename)
   else:
-    print 'unknown option: ' + option
+    print ('unknown option:%s '%(option))
     sys.exit(1)
 
 if __name__ == '__main__':
